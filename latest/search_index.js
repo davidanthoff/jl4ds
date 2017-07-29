a7dc1aad6ca2e33704aac89867e3fc9e5d10fba5
autodocs <autodocs>
2017-07-29 05:23:00 +0000

    build based on 7209623

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Julia for Data Science",
+    "title": "Julia for Data Science",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Julia-for-Data-Science-1",
+    "page": "Julia for Data Science",
+    "title": "Julia for Data Science",
+    "category": "section",
+    "text": ""
+},
+
+]}
