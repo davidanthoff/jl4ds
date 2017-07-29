9b1f7761a9362729b8080e833c38527b41783d56
David Anthoff <anthoff@berkeley.edu>
2017-07-28 22:20:47 -0700

    Add make.jl

---
+++ make.jl
@@ -0,0 +1,17 @@
+using Documenter
+
+makedocs(
+    format = :html,
+    sitename = "Julia for Data Science",
+    pages = [
+        "index.md"
+    ]
+)
+
+deploydocs(
+    repo   = "github.com/davidanthoff/jl4ds",
+    target = "build",
+    deps   = nothing,
+    make   = nothing,
+    julia  = "0.6"
+)
