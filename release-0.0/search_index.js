966737b3529ee549a4cd8b49e5af6ea11f0bda12
autodocs <autodocs>
2017-07-29 05:42:18 +0000

    build based on 8713323

---
+++ release-0.0/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Julia for Data Science",
+    "title": "Julia for Data Science",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Julia-for-Data-Science-1",
+    "page": "Julia for Data Science",
+    "title": "Julia for Data Science",
+    "category": "section",
+    "text": "This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/."
+},
+
+]}
