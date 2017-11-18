9e58aca32710b57bc41fa771368cf7cd300902d8
autodocs <autodocs>
2017-11-18 23:30:18 +0000

    build based on 38d78c2

--- latest/search_index.js
+++ latest/search_index.js
@@ -17,6 +17,38 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "setup.html#",
+    "page": "Setup",
+    "title": "Setup",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "setup.html#Setup-1",
+    "page": "Setup",
+    "title": "Setup",
+    "category": "section",
+    "text": "All you need to follow the examples in this book is a working installation of the programming language julia and the Dataverse.jl package. While that is technically all you need, I would also recommend that you install VS Code with the VS Code julia extension and IJulia, the julia kernel for Jupyter."
+},
+
+{
+    "location": "setup.html#Installing-julia-1",
+    "page": "Setup",
+    "title": "Installing julia",
+    "category": "section",
+    "text": "Installing julia is easy: you just download a version for your computer from the julia language website and then install it on your machine. Once you have installed julia, try to start it. You should see the julia REPL: a command line at which you can enter julia code and execute it by pressing Enter.All examples in this book are based on julia 0.6.x."
+},
+
+{
+    "location": "setup.html#Installing-the-Dataverse.jl-package-1",
+    "page": "Setup",
+    "title": "Installing the Dataverse.jl package",
+    "category": "section",
+    "text": "Once julia is installed, just start it and enter the following command at the REPL:julia> Pkg.clone(\"https://github.com/davidanthoff/Dataverse.jl\")This will install the Dataverse.jl package onto your system. The Dataverse.jl package is a meta-package that pulls in a large number of other packages that you need to run the code examples in this book. You could install all of these packages one-by-one manually, but it is much easier to just install (and later use) the Dataverse.jl package instead and not worry about those details.note: Note\nThe name \"Dataverse\" is not final and I am looking for a better name. Ideas and suggestions are welcome!"
+},
+
+{
     "location": "fileio.html#",
     "page": "Tabular File IO",
     "title": "Tabular File IO",
@@ -45,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Tabular File IO",
     "title": "Loading Data",
     "category": "section",
-    "text": "The main function for reading tabular data from disc is the load function from the FileIO.jl package. You always pass a filename as the first argument to the load function. For some file formats you can also pass additional arguments that control the details of how the data is loaded. You will learn about those additional arguments later in the chapter.It is often convenient to materialize the data from a tabular file into a data structure like a DataFrame. You can do that easily by passing the return value from the load function to the DataFrame constructor, like this:using Dataverse\n\ndf = DataFrame(load(\"mydata.csv\"))You can also use the pipe syntax to achieve the same result:using Dataverse\n\ndf = load(\"mydata.csv\") |> DataFrameThe pipe syntax is particularly useful when you want to apply some data transformation to the data that you are loading. For example, you can filter the data before you materialize into a DataFrame like this:using Dataverse\n\ndf = load(\"mydata.csv\") |> @filter(_.age>20) |> DataFrameThe load function can load many different tabular file formats. The following code loads an Excel file:using Dataverse\n\ndf = load(\"mydata.xlsx\", \"Sheet1\") |> DataFrameNote how we have to pass the name of the sheet to read as a second argument to the load function for Excel files.A full list of supported file formats is provided later in this chapter."
+    "text": "The main function for reading tabular data from disc is the load function from the FileIO.jl package. You always pass a filename as the first argument to the load function. For some file formats you can also pass additional arguments that control the details of how the data is loaded. You will learn about those additional arguments later in the chapter.It is often convenient to materialize the data from a tabular file into a data structure like a DataFrame. You can do that easily by passing the return value from the load function to the DataFrame constructor, like this:using Dataverse\n\ndf = DataFrame(load(\"mydata.csv\"))You can also use the pipe syntax to achieve the same result:using Dataverse\n\ndf = load(\"mydata.csv\") |> DataFrameThe pipe syntax is particularly useful when you want to apply some data transformation to the data that you are loading. For example, you can filter the data before you materialize it into a DataFrame like this:using Dataverse\n\ndf = load(\"mydata.csv\") |> @filter(_.age>20) |> DataFrameThe load function can load many different tabular file formats. The following code loads an Excel file:using Dataverse\n\ndf = load(\"mydata.xlsx\", \"Sheet1\") |> DataFrameNote how we have to pass the name of the sheet to read as a second argument to the load function for Excel files.A full list of supported file formats is provided later in this chapter.You can also use the load function to acquire data from a remote server by passing a URI as the filename. The following code loads a CSV file from a remote server:using Dataverse\n\ndf = load(\"https://raw.githubusercontent.com/davidanthoff/CSVFiles.jl/master/test/data.csv\") |> DataFrame"
 },
 
 {
@@ -53,7 +85,47 @@ var documenterSearchIndex = {"docs": [
     "page": "Tabular File IO",
     "title": "Saving Data",
     "category": "section",
-    "text": "The save function from the FileIO.jl package is the main function to save tabular data to disc. The first argument to the save function is the filename you want to use for the data. The file extension of that filename will determine in what format the data will be written to disc. The second argument is the table you want to write to disc. Here is a simple example that writes some data to a CSV file:using Dataverse\n\ndf = DataFrame(Name=[\"Jim\", \"Sally\", \"John\"], Age=[23., 56., 34.])\n\nsave(\"mydata.csv\", df)You can also use the pipe syntax with the save function:using Dataverse\n\ndf = DataFrame(Name=[\"Jim\", \"Sally\", \"John\"], Age=[23., 56., 34.])\n\ndf |> save(\"mydata.csv\")The save function works with any tabular data structure, not just DataFrames and it supports many different file formats. The following code shows how you can load data from a CSV file, filter it and then write it out directly as a Feather file, without ever materializing it into a DataFrame:using Dataverse\n\nload(\"mydata.csv\") |> @filter(_.age>23) |> save(\"mydata.feather\")"
+    "text": "The save function from the FileIO.jl package is the main function to save tabular data to disc. The first argument to the save function is the filename you want to use for the file. The file extension of that filename will determine in what format the data will be written to disc. The second argument is the table you want to write to disc. Here is a simple example that writes some data to a CSV file:using Dataverse\n\ndf = DataFrame(Name=[\"Jim\", \"Sally\", \"John\"], Age=[23., 56., 34.])\n\nsave(\"mydata.csv\", df)You can also use the pipe syntax with the save function:using Dataverse\n\ndf = DataFrame(Name=[\"Jim\", \"Sally\", \"John\"], Age=[23., 56., 34.])\n\ndf |> save(\"mydata.csv\")The save function works with any tabular data structure, not just DataFrames and it supports many different file formats. The following code shows how you can load data from a CSV file, filter it and then write it out directly as a Feather file, without ever materializing it into a DataFrame:using Dataverse\n\nload(\"mydata.csv\") |> @filter(_.age>23) |> save(\"mydata.feather\")For some file formats you can pass additional configuration arguments to the save function that control in detail how the file is written to disc. The following example writes a table to disc as a CSV file, but uses a non-standard delimeter character and also does not write a header to the file:using Dataverse\n\ndf = DataFrame(Name=[\"Jim\", \"Sally\", \"John\"], Age=[23., 56., 34.])\n\ndf |> save(\"mydata.csv\", delim=';', header=false)"
+},
+
+{
+    "location": "fileio.html#File-Formats-1",
+    "page": "Tabular File IO",
+    "title": "File Formats",
+    "category": "section",
+    "text": "This section will teach you about a number of important file formats in the data science world and how to use them with the load and save function in julia."
+},
+
+{
+    "location": "fileio.html#CSV-Files-1",
+    "page": "Tabular File IO",
+    "title": "CSV Files",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "fileio.html#Feather-Files-1",
+    "page": "Tabular File IO",
+    "title": "Feather Files",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "fileio.html#Excel-Files-1",
+    "page": "Tabular File IO",
+    "title": "Excel Files",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "fileio.html#Stata,-SPSS,-and-SAS-Files-1",
+    "page": "Tabular File IO",
+    "title": "Stata, SPSS, and SAS Files",
+    "category": "section",
+    "text": "[TODO]"
 },
 
 {
