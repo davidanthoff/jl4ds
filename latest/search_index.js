76ac3d4671ab49205be894ebf98431e6884cccaa
autodocs <autodocs>
2017-11-19 05:47:40 +0000

    build based on 7f52b00

--- latest/search_index.js
+++ latest/search_index.js
@@ -89,6 +89,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "fileio.html#The-load-and-save-function-1",
+    "page": "Tabular File IO",
+    "title": "The load and save function",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
     "location": "fileio.html#CSV-Files-1",
     "page": "Tabular File IO",
     "title": "CSV Files",
@@ -109,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Tabular File IO",
     "title": "Specifying a different delimter character",
     "category": "section",
-    "text": "By default CSV files use a comma (,) to separate content in different columns. While that is the most common case, CSV files often use a different character to separate content in different columns. For example, you might want to read a file like this example that uses a semicolon (;) to separate columns:Name;Age\nJohn;34\nSally;52You can tell load to use a different character as the delimiter between columns by passing a Char value as the second argument to the load function:using Dataverse\n\ndf = load(\"mycsvfile_with_semicolon.csv\", ';') |> DataFrameYou can tell load to use any character as the column delimiter signal. Another common case besides the semicolon is a tab charachter, which you can pass easily enter as '\\t'."
+    "text": "By default, CSV files use a comma (,) to separate content in different columns. While that is the most common case, CSV files also sometimes use a different character to separate content in different columns. For example, you might want to read a file that uses a semicolon (;) to separate columns, like the following example:Name;Age\nJohn;34\nSally;52You can tell load to use a different character as the delimiter between columns by passing a Char value as the second argument to the load function:using Dataverse\n\ndf = load(\"mycsvfile_with_semicolon.csv\", ';') |> DataFrameYou can tell load to use any character as the column delimiter signal. Another common case besides the semicolon is a tab character (written as '\\t' in julia)."
 },
 
 {
@@ -117,7 +125,31 @@ var documenterSearchIndex = {"docs": [
     "page": "Tabular File IO",
     "title": "Column Names",
     "category": "section",
-    "text": ""
+    "text": "In most CSV files the first line contains the names of the columns, and subsequent lines the actual data itself. If you call load with no special arguments, it will assume that the first line in the CSV file holds column names. An example of such a CSV file might look like this:Name,Age,Children\n\"John\",23.,1\n\"Sally\",54.,3But sometimes CSV files don't have a special header row with the column names, and instead start with the actual data in the first row, like in this file:\"John\",23.,1\n\"Sally\",54.,3You can indicate this situation by calling the load function with the keyword argument header_exists=false:using Dataverse\n\ndf = load(\"myfile.csv\", header_exists=false) |> DataFrameThe header_exists=false keyword argument will cause two things: the first row of the file will now be read as data, i.e. the resulting table will have a first row with data from the first line in the file. Second, the columns will be named by numbers, i.e. the name of the first column will be 1, the name of the second column 2 and so on, unless you specify custom column names with the colnames keyword argument.The colnames keyword argument of the load function allows you to specify your own column names. You can use that option to either specify the names of all columns as an array of Strings, or you can change the name of only a few columns by passing a Dict.When you pass an array of Strings, you indicate that you want the names in the array to be used as the column names in the resulting table. The following code loads a CSV file and specifies custom column names:using Dataverse\n\ndf = load(\"mydata.csv\", colnames=[\"name\", \"age\", \"children\"]) |> DataFrameWhen you use the colnames argument with header_exists=true (or don't specify that keyword argument), the names in colnames will replace the names that are loaded from the file.Sometimes you load some data from a CSV file that has a column header and you want to replace the names of just a few columns. While you could pass an array of Strings to the colnames argument, it would cumbersome: you would have to specify the names of all columns, even the ones that you don't want to rename. In this situation you can pass a Dict to the colnames argument instead. Each element in the Dict is one renaming rule that load should apply to the columns it loads from the file. The key for each element specifies which column should be renamed, and the value the new name. The key can be specified either as a String, in which case it refers to the column name that is present in the file, or as an Int, in which case it refers to the position of the column that should be renamed. The values in the Dict always have to be Strings, i.e. the new names. Note that you cannot pass a Dict to colnames when you call load with header_exists=false. The following code example will load a CSV file, and rename the column with the original name \"Age\" to \"age\", and the third column to \"children\". All other columns will keep the names that are specified in the file:using Dataverse\n\ndf = load(\"mydata.csv\", colnames=Dict(\"Age\"=>\"age\", 3=>\"children\"))"
+},
+
+{
+    "location": "fileio.html#Rows-to-Read-1",
+    "page": "Tabular File IO",
+    "title": "Rows to Read",
+    "category": "section",
+    "text": "load accepts two keyword arguments that allow you to specify whether all lines in the file should be read or not.With the skiplines_begin argument you can tell load to ignore a certain number of lines at the beginning of the file. This is useful if you have a file like this:# This file was generated on 1/1/2017\n# By John\nName,Age,Children\n\"John\",34.,2\n\"Sally\",23.,1In this example the first two lines in the file contain some meta information that is not part of the table data itself. You can load such a file like this:using Dataverse\n\ndf = load(\"mydata.csv\", skiplines_begin=2) |> DataFrameWith that option, the first two lines will be ignored and the file is treated as if it started in line 3.[TODO There should actually be an option to limit the number of rows that are read]"
+},
+
+{
+    "location": "fileio.html#Column-Types-1",
+    "page": "Tabular File IO",
+    "title": "Column Types",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "fileio.html#Quote-and-Escape-Character-1",
+    "page": "Tabular File IO",
+    "title": "Quote and Escape Character",
+    "category": "section",
+    "text": "If a CSV file has a column with string data, it should ideally surround the actual string in quotation marks \" or some other quote character. This is important because otherwise such a string column could not contain the character that is used as the delimiter character between columns. A typical example CSV file might look like this:Name,Age\n\"Smith, John\",23.\n\"Smith, Sally\",35.Note that the values in the Name column here contain a comma , which is also the delimiter character between columns in this file. But because the whole string is surrounded by quotation marks \", the CSV reader understands that the comma between the last and first name here is part of the Name column and does not separate the Name from the Age column.Some CSV files use a different character as their quotation character. For example, a file might use single quotation marks ' like in this example:Name,Age\n'Smith, John',23.\n'Smith, Sally',35.The keyword argument quotechar of the load function allows you to specify the quote character used in the file you want to load. The above file could be loaded like this:using Dataverse\n\ndf = load(\"mydata.csv\", quotechar='\\'') |> DataFrameNote how we need to use the julia escape character \\ here to create a Char instance with content '.There is still a problem, though: what if you have a column that sometimes contains the character that is used as the quote character? For that case you can specify an escape character: whenever the escape character followed by the quote character appears in a column, the quote character is not interpreted as the end of the column, but as an appearance of that character in the column itself. An example file might look like this:Text,Number\n\"This text contains a \\\" mark\",23\n\"This line doesn't\",45The content of the first column in the first row here should be read as This text contains a \" mark. You can specify what character is used as the escape character with the escapechar keyword argument:using Dataverse\n\ndf = load(\"mydata.csv\", escapechar='\\\\')Note how we have to escape the \\ character itself in the julia string: \\ is the julia escape character, and to create a Char instance with content \\ we have to write '\\\\'."
 },
 
 {
